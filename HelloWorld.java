58a73bb60cf745e1fdcc098c08eadccaccc01f3b
jonamueller <73890152+jonamueller@users.noreply.github.com>
2020-11-08 15:30:32 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {    
-        System.out.println("2");
+        
+   int Anfangsjahr=2018;
+        int Aktuelles_Jahr=2020;
+        int Spanne= Aktuelles_Jahr-Anfangsjahr
+            System.out.print(Spanne);
+        System.out.println("\n2");
+        
     }
 }
